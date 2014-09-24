b40f7b7778b104c8128076c44fbabb3ba642080a
Fabian Gysel <fabiangysel15@gmail.com>
2014-09-24 16:55:03 +0200

    Login und titel design hinzugefügt

--- index.php
+++ index.php
@@ -8,6 +8,18 @@
     </head>
     <body>
         <!--Titel-->
-        <h1 class="title">Willkommen bei F0rum</h1>
+        <div class="title_box" >
+            <h1 class="title">Willkommen bei F0rum</h1>
+
+            <div class="login">
+                <form action="login.php" method="POST">
+                    <input type="text" name="username" placeholder="Username"><br>
+                    <input type="password" name="password" placeholder="Password"><br>
+                    <input type="submit" name="submit" value="Sign in" class="small_button">
+                    <a href="register.php"><input type="button" value="Sign up" class="small_button"></a>
+                </form>
+            </div>
+            <div class="clear"></div>
+        </div>
     </body>
 </html>
