aa0c49b8d8e0739c83a7958519b74fa89fbb6958
Fabian Gysel Offline <Fabian Gysel Offline@Fabians_PC>
2014-10-20 11:45:26 +0200

    filter weiter gemacht

--- Business/filter.php
+++ Business/filter.php
@@ -14,10 +14,10 @@ if($fav_color == "") {
     $fav_color = "SELECT fav_color FROM nachrichten";
 }
 */
-$where = "WHERE n.lang = 'DE'";
+$where = "WHERE u.lang = 'DE'";
 
 $select_ausgabe = select_filter($where);
-echo $select_ausgabe;
+
 while ($select_row = mysql_fetch_object($select_ausgabe)) {
     $ago = $select_row->erstellt_am;
     $_SESSION['nachrichten_id'] = "$select_row->nachrichten_id";
