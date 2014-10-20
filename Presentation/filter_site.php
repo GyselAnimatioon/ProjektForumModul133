aa33bd21a172467513544e322dc9fe392c2ac7db
Timothe <Timothe@Timo-PC.tbz.local>
2014-10-20 11:04:05 +0200

    Filter funktion in html datei fertig

--- Presentation/filter_site.php
+++ Presentation/filter_site.php
@@ -1,7 +1,21 @@
 <div class="filter">
     <form action="../index.php?folder=Business&page=filter.php" method="POST">
-        <input type="text" name="lang" placeholder="Sprache">
-        <input type="text" name="fav_color" placeholder="Lieblingsfarbe">
+        <input type="text" name="username" placeholder="Username"><br>
+        <input type="text" name="vorname" placeholder="Vorname"><br>
+        <input type="text" name="nachname" placeholder="Nachname"><br>
+        <input type="text" name="email" placeholder="EMail"><br>
+        <input type="text" name="tel" placeholder="Telefonnr."><br>
+        <input type="text" name="bday" placeholder="Geburtsdatum"><br>
+        <input type="text" name="bio" placeholder="Biographie"><br>
+        <input type="text" name="fav_color" placeholder="Lieblings Farbe"><br>
+        <input type="text" name="fav_animal" placeholder="Lieblings Tier"><br>
+        <input type="text" name="fav_food" placeholder="Lieblings Essen"><br>
+        <input type="text" name="fav_drink" placeholder="Lieblings Getränk"><br>
+        <input type="text" name="facebook" placeholder="Facebook"><br>
+        <input type="text" name="youTube" placeholder="YouTube"><br>
+        <input type="text" name="twitter" placeholder="Twitter"><br>
+        <input type="text" name="instagram" placeholder="Instagram"><br>
+        <input type="text" name="sprache" placeholder="Sprache">
         <input type="submit" value="Anwenden!">
     </form> 
 </div>
