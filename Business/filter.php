d2818f5d0d6ffcec8a655c5c4aa38434f5f015ed
Timothe <Timothe@Timo-PC.tbz.local>
2014-10-20 11:35:08 +0200

    Filter funktion code angefangen

--- Business/filter.php
+++ Business/filter.php
@@ -4,19 +4,20 @@ require_once 'Business/functions.php';
 require_once 'Data/filter_sql.php';
 require_once 'Presentation/filter_site.php';
 
-
+/*
 $lang = filter_input(INPUT_POST, "lang");
 $fav_color = filter_input(INPUT_POST, "fav_color");
 if($lang == "") {
-    $lang = "de','en','fr";
+    $lang = "'de','en','fr'";
 }
 if($fav_color == "") {
-    $lang = "*";
+    $fav_color = "SELECT fav_color FROM nachrichten";
 }
+*/
+$where = "WHERE n.lang = 'DE'";
 
-
-$select_ausgabe = select_filter($lang, $fav_color);
-
+$select_ausgabe = select_filter($where);
+echo $select_ausgabe;
 while ($select_row = mysql_fetch_object($select_ausgabe)) {
     $ago = $select_row->erstellt_am;
     $_SESSION['nachrichten_id'] = "$select_row->nachrichten_id";
--- Data/filter_sql.php
+++ Data/filter_sql.php
@@ -1,14 +1,14 @@
 <?php
 
-function select_filter($lang, $fav_color) {
+function select_filter($where) {
+    echo $where . "<br>";
     $_SESSION['site'] = "folder=Business&page=filter.php";
     $select_abfrage = "SELECT * "
             . "FROM gefaellt_mir g "
             . "INNER JOIN nachrichten n ON n.id = g.nachrichten_id "
             . "INNER JOIN user_nachricht un ON un.nachrichten_id = n.id "
             . "INNER JOIN user u ON u.id = un.user_id "
-            . "WHERE u.lang in ('$lang') AND "
-            . "u.fav_color in ('$fav_color') "
+            . "$where "
             . "GROUP BY n.id";
     echo $select_abfrage;
     $select_ausgabe = mysql_query($select_abfrage);
